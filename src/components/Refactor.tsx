5317161ddf8298b29e6787243d7e7c4995f3ca93
muzi55555 <muzi@lineus.kr>
2024-09-10 13:48:51 +0900

    스로틀 적용

--- src/components/Refactor.tsx
+++ src/components/Refactor.tsx
@@ -9,9 +9,35 @@ interface IPosition {
 export default function Refactor() {
   const [mousePosition, setMousePosition] = useState<IPosition>({ x: 0, y: 0 });
 
-  const handleMousePosition = (e: MouseEvent) => {
-    setMousePosition({ x: e.clientX, y: e.clientY });
+  /**
+   * 주어진 함수를 지정된 시간 간격으로 호출하는 쓰로틀링 함수입니다.
+   * @param func 호출할 함수
+   * @param limit 호출 간격 제한 시간 (밀리초)
+   * @returns 쓰로틀링된 함수
+   */
+  const throttle = (func: any, limit: number) => {
+    let lastFunc: number;
+    let lastRan: number;
+    return function (...args: any[]) {
+      if (!lastRan) {
+        func(...args);
+        lastRan = Date.now();
+      } else {
+        clearTimeout(lastFunc);
+        lastFunc = window.setTimeout(() => {
+          if (Date.now() - lastRan >= limit) {
+            func(...args);
+            lastRan = Date.now();
+          }
+        }, limit - (Date.now() - lastRan));
+      }
+    };
   };
+
+  const handleMousePosition = throttle((e: MouseEvent) => {
+    console.log(1);
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  }, 100);
 
   useEffect(() => {
     window.addEventListener('mousemove', handleMousePosition);
@@ -22,7 +48,6 @@ export default function Refactor() {
 
   return (
     <div className={styles.eventBox}>
-      {/* <div className={styles.contentBox} style={{ top: mousePosition.y, left: mousePosition.x }}> */}
       <div
         className={styles.contentBox}
         style={{ transform: `translate(calc(${mousePosition.x}px - 50%), calc(${mousePosition.y}px - 50%))` }}
