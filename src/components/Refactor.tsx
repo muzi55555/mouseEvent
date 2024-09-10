aaa23b2dfeb50f090f7371f732ad40e5fec62a58
muzi55555 <muzi@lineus.kr>
2024-09-10 14:54:49 +0900

    requestAnimationFrame

--- src/components/Refactor.tsx
+++ src/components/Refactor.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './MouseEventComponenet.module.css';
 
 interface IPosition {
@@ -8,41 +8,25 @@ interface IPosition {
 
 export default function Refactor() {
   const [mousePosition, setMousePosition] = useState<IPosition>({ x: 0, y: 0 });
+  const requestRef = useRef<number>();
 
-  /**
-   * 주어진 함수를 지정된 시간 간격으로 호출하는 쓰로틀링 함수입니다.
-   * @param func 호출할 함수
-   * @param limit 호출 간격 제한 시간 (밀리초)
-   * @returns 쓰로틀링된 함수
-   */
-  const throttle = (func: any, limit: number) => {
-    let lastFunc: number;
-    let lastRan: number;
-    return function (...args: any[]) {
-      if (!lastRan) {
-        func(...args);
-        lastRan = Date.now();
-      } else {
-        clearTimeout(lastFunc);
-        lastFunc = window.setTimeout(() => {
-          if (Date.now() - lastRan >= limit) {
-            func(...args);
-            lastRan = Date.now();
-          }
-        }, limit - (Date.now() - lastRan));
-      }
+  const handleMousePosition = (e: MouseEvent) => {
+    const updatePosition = () => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
     };
+    if (requestRef.current) {
+      cancelAnimationFrame(requestRef.current);
+    }
+    requestRef.current = requestAnimationFrame(updatePosition);
   };
-
-  const handleMousePosition = throttle((e: MouseEvent) => {
-    console.log(1);
-    setMousePosition({ x: e.clientX, y: e.clientY });
-  }, 100);
 
   useEffect(() => {
     window.addEventListener('mousemove', handleMousePosition);
     return () => {
       window.removeEventListener('mousemove', handleMousePosition);
+      if (requestRef.current) {
+        cancelAnimationFrame(requestRef.current);
+      }
     };
   }, []);
 
@@ -50,7 +34,9 @@ export default function Refactor() {
     <div className={styles.eventBox}>
       <div
         className={styles.contentBox}
-        style={{ transform: `translate(calc(${mousePosition.x}px - 50%), calc(${mousePosition.y}px - 50%))` }}
+        style={{
+          transform: `translate(calc(${mousePosition.x}px - 50%), calc(${mousePosition.y}px - 50%))`,
+        }}
       >
         마우스이벤트
       </div>
